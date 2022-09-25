40422e2decbce6ceeccf1d0fa2a4038df8a570e5
jchilikova <jchilikova@gmail.com>
2022-09-25 17:31:25 +0300

    Added return error for Withdraw and Deposit when the amount passed is negative and fixed some of the unit tests

--- BankingKata/Account.cs
+++ BankingKata/Account.cs
@@ -10,6 +10,11 @@ namespace BankingKata
 
         public BalanceResult Deposit(decimal amount)
         {
+            if (CheckIfAmountIsNegative(amount))
+            {
+                return GetNegativeAmountError();
+            }
+
             return new BalanceResult()
             {
                 Balance = Math.Round(Balance += amount, round),
@@ -19,10 +24,44 @@ namespace BankingKata
 
         public BalanceResult Withdraw(decimal amount)
         {
+            var currentBalance = Balance;
+
+            if (CheckIfAmountIsNegative(amount))
+            {
+                return GetNegativeAmountError();
+            }
+            
+            var result = Math.Round(currentBalance -= amount, round);
+
+            if (CheckIfAmountIsNegative(result))
+            {
+                return new BalanceResult()
+                {
+                    Balance = Balance,
+                    Error = ErrorType.InsufficientFunds
+                };
+            }
+
+            this.Balance = result;
+
             return new BalanceResult()
             {
-                Balance = Math.Round(Balance -= amount, round),
+                Balance = result,
                 Error = null
+            };
+        }
+
+        private bool CheckIfAmountIsNegative(decimal amount)
+        {
+            return amount < 0;
+        }
+
+        private BalanceResult GetNegativeAmountError()
+        {
+            return new BalanceResult()
+            {
+                Balance = Balance,
+                Error = ErrorType.NegativeAmount
             };
         }
     }
